dd57bad68aa5041a2888fd62c7dc208f2cc0e9c2
Michal-Z9 <michalzar.2009@gmail.com>
2025-09-14 15:59:16 +0200

    Pierwszy commit kalkulatora

---
+++ kalkulator-1.py
@@ -0,0 +1,26 @@
+
+
+while True:
+    a = float(input("podaj pierwsza liczbe:"));
+    b = float(input("podaj druga liczbe:"));
+    d = input("podaj typ działania:");
+    if d=="+":
+        k=a+b;
+
+    elif d=="-":
+        k=a-b;
+
+    elif d=="*":
+        k=a*b;
+
+    elif d=="/":
+        if b!=0:
+            k=a/b;
+        else:
+            print("nie mozna wykonac zadania")
+    else:
+        k=("nie znane dzialanie")
+    print(a,d,b,"=",k);
+
+
+2
